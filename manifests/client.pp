4d32717c320e17b2ec64b97466486fbde8ca7c2c
Markus Kont <markuskont@gmail.com>
2016-01-19 16:57:49 +0200

    use ip for now.

--- manifests/client.pp
+++ manifests/client.pp
@@ -26,7 +26,7 @@ class nagios::client (
   @@nagios_host { $fqdn:
     ensure        =>  present,
     alias         =>  $::hostname,
-    address       =>  $::ipaddress,
+    address       =>  $::ip,
     use           =>  "generic-host",
     target        =>  $target,
     owner         =>  $user,
