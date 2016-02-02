e38dc1240fbc1de59b940a7b6c48c21d0bd955f8
Markus Kont <markuskont@gmail.com>
2016-02-02 16:35:40 +0200

    Create fact for block device driver. For generating nagios raid monitoring resources

--- lib/facter/identify_disks.rb
+++ lib/facter/identify_disks.rb
@@ -1,5 +1,18 @@
 #require 'pathname'
-#
+require 'open3'
+
+def parse_udev_driver(device)
+  driver = ''
+  cmd = "udevadm info -a -n #{device}"
+  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+    while line = stdout.gets
+      if /DRIVERS=="(megaraid_sas)"/.match(line)
+        return $1
+      end
+    end
+  end
+end
+
 def parse_blockdev_dir()
   sysfs_block_directory = '/sys/block/'
   blockdevices = {}
@@ -7,7 +20,12 @@ def parse_blockdev_dir()
   if File.exist?(sysfs_block_directory)
     Dir.entries(sysfs_block_directory).each do |device|
       if (device =~ /^md\d+/)
-        (blockdevices["mdadm"] ||= []) << device
+        driver = 'mdadm'
+      else
+        driver = parse_udev_driver(device)
+      end
+      if driver
+        (blockdevices[driver] ||= []) << device
       end
     end
   end
