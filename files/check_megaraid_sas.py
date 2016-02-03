b92eb982db958b6b7732f69d480b0f382d8f6bc2
Markus Kont <markuskont@gmail.com>
2016-02-03 19:22:11 +0200

    Improvements for check script. Now contains basic check functionality.

--- files/check_megaraid_sas.py
+++ files/check_megaraid_sas.py
@@ -38,15 +38,30 @@ def getAdapters():
   return adapters
 
 def getLdStatus(adapters):
+  nagios_status = {}
+  code = 0
+  message = ''
+  regex_ld = re.compile('Virtual Drive: (\d+)')
+  regex_state = re.compile('State\s*:\s*(\w+)')
   for a in adapters:
-    a = "-A%s" % a
+    message = message + ' Adp: ' + a
+    opt = "-A%s" % a
     try:
-      ld_info = subprocess.Popen(['sudo', MEGACLIBIN, '-LDInfo', '-LAll', a], stdout=subprocess.PIPE)
+      ld_info = subprocess.Popen(['sudo', MEGACLIBIN, '-LDInfo', '-LAll', opt], stdout=subprocess.PIPE)
     except:
       nagiosReturn(3, "Unable to gather logical device info for adapter %s")
     for line in ld_info.stdout:
-      print line,
-  return ld_info
+      if regex_state.match(line):
+        status = regex_state.match(line).group(1)
+        message = message +' State: ' + status
+        if status != 'Optimal':
+          code = 2
+      elif regex_ld.match(line):
+        message = message +' LD: ' + regex_ld.match(line).group(1)
+        #print line,
+  nagios_status['code'] = code
+  nagios_status['message'] = message
+  return nagios_status
 
 # GLOBAL VARIABLES
 MEGACLIBIN = parseArguments().binary
@@ -58,7 +73,8 @@ def main():
     nagiosReturn(3, message)
   else:
     adapters = getAdapters()
-  getLdStatus(adapters)
+  ld_info = getLdStatus(adapters)
+  nagiosReturn(ld_info['code'], ld_info['message'])
 
 if __name__ == "__main__":
   main()
