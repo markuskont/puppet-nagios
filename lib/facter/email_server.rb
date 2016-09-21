3673b4aabadfbf95cedd30cf8e6211662fcfeee7
Markus <markus@spin.ee>
2016-09-21 13:41:34 +0300

    Sendmail and postfix binaries can be both present in postfix setups. Only sendmail was reflected in fact

--- lib/facter/email_server.rb
+++ lib/facter/email_server.rb
@@ -4,11 +4,13 @@ Facter.add("email_server") do
   confine :kernel => 'Linux'
   hash = {}
   cmd = "which postfix sendmail"
+  paths = []
 
   Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
     while line = stdout.gets
-      hash['path'] = line.strip
+      paths << line.strip
     end
+    hash['path'] = paths if paths.size > 0
   end
   if ( hash['path'] )
     hash['installed'] = true
